3c4ea1f73a556f1fc2d6f654b5cf4f979123b614
Antitech-K <kononov_mt2@mail.ru>
2022-10-17 22:36:22 +0300

    first

--- scraping_ali.py
+++ scraping_ali.py
@@ -9,7 +9,7 @@ from selenium.webdriver.common.action_chains import ActionChains
 import codecs
 import time
 
-#реклама
+
 opt=Options()
 opt.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36")
 opt.add_argument("--disable-blink-features=AutomationControlled")
